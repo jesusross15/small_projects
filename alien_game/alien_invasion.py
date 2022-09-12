affdb9b3ed7c716d6267ff975842bcf6f0e8a26f
Jesus <jesusross15@gmail.com>
2022-09-12 12:20:36 -0400

    initializing game

---
+++ alien_game/alien_invasion.py
@@ -0,0 +1,35 @@
+import sys
+import pygame
+
+# This class will serve as the superclass to manage game assets and behavior
+class AlienInvasion:
+    def __init__(self):
+        # This method serves as a way to initialize the game and create game resources
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Inavsion")
+        
+        # Setting the background color
+        # This color would be a light gray color (we can play around and change it later if we'd like)
+        self.big_color = (230, 230, 230)
+        
+    def run_game(self):
+        # Starts the main loop for the game
+        while True:
+            # Watches keyboard and mouse "events" (movements)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Fill the screen with the color chose and redraw the screen during each pass I make through the loop
+            self.screen.fill(self.big_color)
+            
+            # Makes the most recently drawn screen visible       
+            pygame.display.flip()
+            
+
+if __name__ == "__main__":
+    # Makes an instance of the game and runs the game 
+    ai = AlienInvasion()
+    ai.run_game()
