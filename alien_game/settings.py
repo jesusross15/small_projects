d9fbe3f4512b759325992f9c501359ec7db7aa61
Jesus <jesusross15@gmail.com>
2022-09-12 12:26:41 -0400

    game settings

---
+++ alien_game/settings.py
@@ -0,0 +1,10 @@
+# This class will serve ad the class that stores all the settings for the game
+class Settings:
+    
+    #Initializing the game's settings
+    def __init__(self):
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
