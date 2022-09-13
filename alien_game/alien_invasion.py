8cc6ac50ba377346e491dbb486d028dcc86fbaca
Jesus <jesusross15@gmail.com>
2022-09-13 19:04:25 -0400

    ship go left and right

--- alien_game/alien_invasion.py
+++ alien_game/alien_invasion.py
@@ -1,5 +1,4 @@
 import sys
-from turtle import Screen
 import pygame
 
 # Imports the settings class
@@ -28,6 +27,7 @@ class AlienInvasion:
         # Starts the main loop for the game
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             
     def _check_events(self):
@@ -35,9 +35,24 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-          
-    def _update_screen(self):        
-        # Fill the screen with the color chose and redraw the screen during each pass I make through the loop
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                
+                
+                          
+    def _update_screen(self):
+        # Update images on the screen, and flip to the new screen        
+        # Redraw the screen during each pass I make through the loop
         self.screen.fill(self.settings.bg_color)
         # From ship class
         self.ship.blitme()
