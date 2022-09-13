8bca98f5e5de0c87f46255de388579b04cc0dd13
Jesus <jesusross15@gmail.com>
2022-09-13 19:38:33 -0400

    changing ship speed and limiting its range

--- alien_game/ship.py
+++ alien_game/ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         # Initialize the ship and set it's starting position.
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         
         # Load the ship image and get its rectangle
@@ -15,17 +16,25 @@ class Ship:
         # Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
         
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+        
         # Movement flags
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         # Update the ship's position based on the movement flags
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # Update the ship's x value, not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            # self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            #self.rect.x -= 1
+            self.x -= self.settings.ship_speed
         
+        # Update rect object from self.x
+        self.rect.x = self.x
         
     def blitme(self):
         # Draws the ship and its current location
