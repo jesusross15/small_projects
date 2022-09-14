0d2fa5a7e635eba9fc147bf8a6aacbe47af63569
Jesus <jesusross15@gmail.com>
2022-09-14 16:06:09 -0400

    making the alien fleet move

--- alien_game/alien_invasion.py
+++ alien_game/alien_invasion.py
@@ -43,6 +43,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             
                             
@@ -92,6 +93,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
             # print(len(self.bullets)) this is to check if the bullets actually disappear from the screen            
+    
+    def _update_aliens(self):
+        # Check if the fleet is at an edge then update the positions of all aliens in the fleet
+        self._check_fleet_edges()
+        self.aliens.update()
                    
     def _create_fleet(self):
         # Create an alien and find the number of aliens in a row.
@@ -121,6 +127,20 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+        
+        
+    def _check_fleet_edges(self):
+        # Respond appropriately if any aliens have reached an edge
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+            
+    def _change_fleet_direction(self):
+        # Drop the entire fleet and change the fleet's direction
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
                           
     def _update_screen(self):
         # Update images on the screen, and flip to the new screen        
