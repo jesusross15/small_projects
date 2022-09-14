1ee9be86ec0f2199c957903c3668afe28241c981
Jesus <jesusross15@gmail.com>
2022-09-14 14:25:57 -0400

    modifying bullets

--- alien_game/alien_invasion.py
+++ alien_game/alien_invasion.py
@@ -35,9 +35,10 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             
+                            
     def _check_events(self):
         # Responds to keyboard and mouse movements
         for event in pygame.event.get():
@@ -67,13 +68,24 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
  
-                
+    
     # Method for fireing bullet
     def _fire_bullet(self):
         # Create a new bullet and add it to the bullets group
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
     
+    
+    def _update_bullets(self):       
+        # Update the bullet's position
+        self.bullets.update()
+        # Get rid of the bullets that have disappeared bc they take up too much memory
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            # print(len(self.bullets)) this is to check if the bullets actually disappear from the screen            
+                   
                           
     def _update_screen(self):
         # Update images on the screen, and flip to the new screen        
